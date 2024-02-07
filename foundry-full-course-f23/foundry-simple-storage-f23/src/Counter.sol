88e1fe16fb413b694b4e81f01c91d587954130cf
0xSecuri <0xtheyoungauditor@gmail.com>
2024-02-07 13:15:53 +0200

    Learned how to deploy contracts using Foundry

--- foundry-full-course-f23/foundry-simple-storage-f23/src/Counter.sol
+++ foundry-full-course-f23/foundry-simple-storage-f23/src/Counter.sol
@@ -2,10 +2,13 @@
 pragma solidity ^0.8.13;
 
 contract Counter {
+    event NumberSetTo(uint256 indexed number);
+
     uint256 public number;
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
+        emit NumberSetTo(newNumber);
     }
 
     function increment() public {
