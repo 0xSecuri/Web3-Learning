dcfa1fa774bd7463ecb12836a70111291f424d61
0xSecuri <0xtheyoungauditor@gmail.com>
2024-02-08 17:29:33 +0200

    Mocking AggregatorV3 for running tests locally

--- foundry-full-course-f23/foundry-fund-me-f23/script/HelperConfig.s.sol
+++ foundry-full-course-f23/foundry-fund-me-f23/script/HelperConfig.s.sol
@@ -2,9 +2,12 @@
 pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 
 contract HelperConfig is Script {
     NetworkConfig public activeNetworkConfig;
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_PRICE = 2000e8;
 
     struct NetworkConfig {
         address priceFeed;
@@ -13,6 +16,8 @@ contract HelperConfig is Script {
     constructor() {
         if (block.chainid == 11155111) {
             activeNetworkConfig = getSepoliaEthConfig();
+        } else {
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
         }
     }
 
@@ -24,5 +29,23 @@ contract HelperConfig is Script {
         sepoliaNetworkConfig = NetworkConfig({
             priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
         });
+    }
+
+    function getOrCreateAnvilEthConfig()
+        public
+        returns (NetworkConfig memory anvilNetworkConfig)
+    {
+        if (anvilNetworkConfig.priceFeed != address(0)) {
+            return anvilNetworkConfig;
+        }
+
+        vm.startBroadcast();
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(
+            DECIMALS,
+            INITIAL_PRICE
+        );
+        vm.stopBroadcast();
+
+        anvilNetworkConfig = NetworkConfig({priceFeed: address(mockPriceFeed)});
     }
 }
